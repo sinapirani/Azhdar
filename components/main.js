dbee0ba2c73c23646d6e650f66b1abbf507227cc
sinapirani <realsinapirani@gmail.com>
2022-03-05 23:19:15 +0330

    init

---
+++ components/main.js
@@ -0,0 +1,11 @@
+import { useState } from "react"
+
+export default function Main({width}){
+
+
+    return(
+        <div style={{'width': width}} className={`bg-blue-500 h-screen`}>
+
+        </div>
+    )
+}
