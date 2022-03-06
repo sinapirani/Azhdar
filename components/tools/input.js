692bc3eca4c63a2c6396764131268bd2be28d9a6
sinapirani <realsinapirani@gmail.com>
2022-03-06 23:48:58 +0330

    input Entr listener deleted

--- components/tools/input.js
+++ components/tools/input.js
@@ -6,15 +6,10 @@ function Input({}) {
 
     const dispatch = useDispatch();
     const code = useSelector(state => state.Code.code);
-    const textAreaKeyDown = (e)=>{
-        if(e.key == 'Enter'){
-            dispatch(setCode(code+'\n'))
-        }
-    }
 
     return (
         <>
-            <textarea onKeyDown={textAreaKeyDown} onChange={(e)=>dispatch(setCode(e.target.value))} className=" w-full h-full px-4 border-1  text-white bg-slate-900 active:border-0" id="input"></textarea>
+            <textarea onChange={(e)=>dispatch(setCode(e.target.value))} className=" w-full h-full px-4 border-1  text-white bg-slate-900 active:border-0" id="input"></textarea>
         </>
     );
 }
