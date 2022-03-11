f19bd37d18cf9d63022d4306679a6f078d9e2b62
sinapirani <realsinapirani@gmail.com>
2022-03-11 12:27:19 +0330

    options styles ok

--- components/tools/bottons/themeBtn.js
+++ components/tools/bottons/themeBtn.js
@@ -7,16 +7,12 @@ export default function ThemeChanger(){
     const width = useSelector((state) => state.Tools.width)
     const theme = useSelector(state=> state.Theme.theme)
     const [themeBackground, setThemeBackground] = useState('') 
-    const themeStyles = useSelector(state=> state.Theme.themeStyles)
-    console.log(themeStyles)
+    const themeStyles = useSelector(state=> state.Theme.themeStyles)    
+    const [showOption,setShowOption] = useState(false)
 
-    const btnStyle = {
-        'width': `${(width/100)*14}px`,
-        height: `${(width/100)*7}px`,
-    }
-    const svgStyle = {
-        'width': `${(width/100)*8}px`,
-        height: `${(width/100)*8}px`,
+
+    const onThemeListClick = ()=>{
+
     }
 
     useEffect(()=>{
@@ -31,6 +27,15 @@ export default function ThemeChanger(){
         }
     },[theme])
 
+    const btnStyle = {
+        'width': `${(width/100)*14}px`,
+        height: `${(width/100)*7}px`,
+    }
+    const svgStyle = {
+        'width': `${(width/100)*8}px`,
+        height: `${(width/100)*8}px`,
+    }
+
     const themeStyle = {
         'background': themeBackground,
         'width': `${(width/100)*4}px`,
@@ -39,20 +44,29 @@ export default function ThemeChanger(){
     }
 
     const absoluteStyle = {
-        'width': `${(width/100)*14}px`,
+        // 'width': `${(width/100)*14}px`,
+        'bottom': `${(width/100)*7}px`,
+
     }
-    const [optionStyle,setOptionStyle] = useState(0)
+    const optionStyle = {
+        'width': `${(width/100)*2.5}px`,
+        'height': `${(width/100)*2.5 }px`,
+    }
+    console.log(optionStyle.width)
 
     return(
         <>
-            <div style={btnStyle} className=" relative z-20 flex justify-center items-center bg-transparent  font-extrabold text-white text-lg  border-2 border-solid border-white/50 rounded-md   hover:text-fuchsia-500 duration-75" id="run">
+            <div style={btnStyle} className=" relative z-50 flex justify-center items-center bg-transparent  font-extrabold text-white text-lg  border-2 border-solid border-white/50 rounded-md cursor-pointer  hover:text-fuchsia-500 duration-75" id="run">
                 <div style={themeStyle} ></div>
-                <div style={absoluteStyle} className="absolute top-0 z-30 bg-white text-black hover:text-black ">
+                <div onClick={onThemeListClick} style={absoluteStyle} className={`absolute rounded-lg px-2 py-2 z-50 bg-white text-black hover:text-black ${showOption? 'block' : 'hidden'}`}>
                     {
-                        themeStyles.map(theme => {
+                        themeStyles.map((theme,key) => {
                             return(
-                                <div>{theme.name}</div>
-                            )
+                                <div className="flex gap-2 justify-center items-center w-22" key={key} >
+                                    <span className="rounded-full" style={{...optionStyle,'background': theme.background}}></span>
+                                    <span className=" font-normal text-sm w-16">{theme.name}</span>
+                                </div>
+                            ) 
                         })
                     }
                 </div>
